e87fcedf83d28e6b47b9b6442187bfbd20ffd0ce
adrian-mendez <64709518+adrian-mendez@users.noreply.github.com>
2021-05-07 17:46:34 -0300

    Pagina hidroponia

---
+++ luz en el camino/main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.nav-main');
+ScrollReveal().reveal('.luz' , { delay: 400 });
+ScrollReveal().reveal('#somos' , { delay: 500 });
+ScrollReveal().reveal('.lechuga' , { delay: 400 });
+ScrollReveal().reveal('.banner' , { delay: 400 });
+ScrollReveal().reveal('.invernadero' , { delay: 400 });
